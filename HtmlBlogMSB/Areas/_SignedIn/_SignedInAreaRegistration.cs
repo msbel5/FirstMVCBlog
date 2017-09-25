26fbf2b2c2aa2f1d3d65b5812427a8b8222a5b34
gizemtanriverdi <30877080+gizemtanriverdi@users.noreply.github.com>
2017-09-25 15:19:33 +0300

    Added SignedIn Area

---
+++ HtmlBlogMSB/Areas/_SignedIn/_SignedInAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace HtmlBlogMSB.Areas._SignedIn
+{
+    public class _SignedInAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "_SignedIn";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "_SignedIn_default",
+                "_SignedIn/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
